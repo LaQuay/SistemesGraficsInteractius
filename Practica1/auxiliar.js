861395427b4fc47a15a73cf22625aca8056b09aa
Marc Vila <marc.vila.gomez@fib.upc.edu>
2017-11-03 18:00:01 +0100

    Pract1: Base code with 2 triangles

---
+++ Practica1/auxiliar.js
@@ -0,0 +1,28 @@
+function drawPyramid(vertices, verticesItemSize, verticesNumItems, colors, colorsItemSize, colorsNumItems) {
+	return drawElement(vertices, verticesItemSize, verticesNumItems, colors, colorsItemSize, colorsNumItems);
+}
+
+function drawElement(vertices, verticesItemSize, verticesNumItems, colors, colorsItemSize, colorsNumItems) {
+	var vertexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	vertexBuffer.itemSize = verticesItemSize;
+	vertexBuffer.numItems = verticesNumItems;
+	
+	var colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+	colorBuffer.itemSize = colorsItemSize;
+	colorBuffer.numItems = colorsNumItems;
+	
+	return {
+		"vertexBuffer": vertexBuffer,
+		"colorBuffer": colorBuffer
+	};
+}
+
+function degToRad(degrees) {
+	return degrees * Math.PI / 180;
+}
