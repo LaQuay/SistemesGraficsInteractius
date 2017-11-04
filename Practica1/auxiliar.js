ce1e133ae9a81e81c9e6bc04f88c5498cc25eca1
Marc <marcvila10@hotmail.com>
2017-11-05 00:10:53 +0100

    Full screen canvas

--- Practica1/auxiliar.js
+++ Practica1/auxiliar.js
@@ -6,3 +6,17 @@ function degToRad(degrees) {
 function customClone(oldObject) {
 	return JSON.parse(JSON.stringify(oldObject));
 }
+
+function resize(canvas) {
+  // Lookup the size the browser is displaying the canvas.
+  var displayWidth  = canvas.clientWidth;
+  var displayHeight = canvas.clientHeight;
+ 
+  // Check if the canvas is not the same size.
+  if (canvas.width  != displayWidth ||
+	  canvas.height != displayHeight) {	 
+	// Make the canvas the same size
+	canvas.width  = displayWidth;
+	canvas.height = displayHeight;
+  }
+}
